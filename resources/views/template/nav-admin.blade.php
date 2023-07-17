a37a7574c0953a55555c653fde3f737d0f02c9ef
Eduardo <picis_9716@hotmail.com>
2023-07-16 23:25:33 -0500

    PAGINA WEB FRUTIMANIA - AJAX update v.1.0 added react

--- resources/views/template/nav-admin.blade.php
+++ resources/views/template/nav-admin.blade.php
@@ -1,29 +1,27 @@
-{{-- NAVEGACION --}}
 <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
     <div class="container">
         <a class="navbar-brand" href="#">Navbar</a>
-        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav"
-            aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent"
+            aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
         </button>
-        <div class="collapse navbar-collapse" id="navbarNav">
-            <ul class="navbar-nav">
-                <li class="nav-item active">
-                    <a class="nav-link" href="{{ route('home.index') }}">Home</a>
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                <li class="nav-item">
+                    <a class="nav-link active" aria-current="page" href="{{ route('home.index') }}">Casa</a>
                 </li>
                 <li class="nav-item">
-                    <a class="nav-link" href="{{ route('supply.index') }}">Supplies</a>
+                    <a class="nav-link" href="{{ route('supply.index') }}">Suministros</a>
                 </li>
                 <li class="nav-item">
-                    <a class="nav-link" href="{{ route('category.index') }}">Categories</a>
+                    <a class="nav-link" href="{{ route('category.index') }}">Categorias</a>
                 </li>
                 <li class="nav-item">
-                    <a class="nav-link" href="{{ route('provider.index') }}">Providers</a>
+                    <a class="nav-link" href="{{ route('provider.index') }}">Proovedores</a>
                 </li>
                 <li class="nav-item">
-                    <a class="nav-link" href="{{ route('juice.index') }}">Juices</a>
+                    <a class="nav-link" href="{{ route('juice.index') }}">Jugos</a>
                 </li>
-
                 <li class="nav-item">
                     <form action="{{ route('logout') }}" method="POST">
                         @csrf
