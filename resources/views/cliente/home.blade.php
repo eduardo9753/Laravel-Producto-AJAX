077c075c0299453c139631f0c34b715d1dc36fc9
Eduardo <picis_9716@hotmail.com>
2023-07-20 16:58:17 -0500

    PAGINA WEB FRUTIMANIA - AJAX update migrations v.1.0

--- resources/views/cliente/home.blade.php
+++ resources/views/cliente/home.blade.php
@@ -42,25 +42,31 @@
     <section class="margen-arriba" id="menu">
         <div class="contenedor">
 
-            <a href="{{ route('menu.show', ['id' => 1]) }}">
-                <div class="menu-fondo imagen-jugo centrar-div">
-                    <h3>jugos</h3>
+            <a href="{{ route('menu.show', ['id'=>1]) }}">
+                <div class="menu-fondo imagen-jugo-personal centrar-div">
+                    <h3>jugos-personal</h3>
                 </div>
             </a>
 
-            <a href="{{ route('menu.show', ['id' => 2]) }}">
-                <div class="menu-fondo imagen-pastel centrar-div">
-                    <h3>pasteles</h3>
+            <a href="{{ route('menu.show', ['id'=>2]) }}">
+                <div class="menu-fondo imagen-jugo-jarra centrar-div">
+                    <h3>jugos-Jarra</h3>
                 </div>
             </a>
 
-            <a href="{{ route('menu.show', ['id' => 3]) }}">
-                <div class="menu-fondo imagen-sandwich centrar-div">
-                    <h3>sandwich</h3>
+            <a href="{{ route('menu.show', ['id'=>3]) }}">
+                <div class="menu-fondo imagen-pastel-dulce centrar-div">
+                    <h3>pasteles-dulces</h3>
                 </div>
             </a>
 
             <a href="{{ route('menu.show', ['id'=>4]) }}">
+                <div class="menu-fondo imagen-pastel-salado centrar-div">
+                    <h3>pasteles-salados</h3>
+                </div>
+            </a>
+
+            <a href="{{ route('menu.show', ['id'=>5]) }}">
                 <div class="menu-fondo imagen-promocion centrar-div">
                     <h3>promociones</h3>
                 </div>
