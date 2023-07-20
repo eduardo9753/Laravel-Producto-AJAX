453abcde02a25ac66c8026e43b1d0a91399a862b
Eduardo <picis_9716@hotmail.com>
2023-07-20 16:18:00 -0500

    PAGINA WEB FRUTIMANIA - AJAX update migrations v.1.0

--- public/js/ajaxJuice.js
+++ public/js/ajaxJuice.js
@@ -66,7 +66,7 @@ $(function () {
             $(edit_juice_modal).find('#edit-juice-form').find('#nombre').val(data.result.nombre);
             $(edit_juice_modal).find('#edit-juice-form').find('#precio').val(data.result.precio);
             //se agregar el tipo del producto y debajo se recorre el select
-            $(edit_juice_modal).find('#edit-juice-form').find('#type_id').append("<option value='"+data.result.type_id+"'>"+data.result.nombre_tipo+"</option>");
+            //$(edit_juice_modal).find('#edit-juice-form').find('#type_id').append("<option value='"+data.result.type_id+"'>"+data.result.nombre_tipo+"</option>");
             $(edit_juice_modal).find('#edit-juice-form').find('.img-old').attr('src', '/productos/' + data.result.imagen);
             $(edit_juice_modal).find('#edit-juice-form').find('#descripcion').val(data.result.descripcion);
             //mostrando el modal
