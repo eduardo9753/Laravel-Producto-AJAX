32e1218908eeb0d6e4b539cafe9de8a29f40abcd
Eduardo <picis_9716@hotmail.com>
2023-09-27 12:19:23 -0500

    PAGINA WEB FRUTIMANIA - AJAX added mercadopago - stripe v.1.5

--- app/Http/Controllers/admin/mercadopago/MercadoPagoWebHookController.php
+++ app/Http/Controllers/admin/mercadopago/MercadoPagoWebHookController.php
@@ -15,7 +15,7 @@ class MercadoPagoWebHookController extends Controller
         $dataArray = json_decode($jsonData, true); // Convierte la cadena JSON en un arreglo asociativo
 
         Log::info('Datos tipo de comprar'. $dataArray['type']);
-        /* Asumiendo que el evento del webhook es un pago exitoso
+        /*Asumiendo que el evento del webhook es un pago exitoso
         if ($dataArray['type'] === 'payment') {
             $id_pago = $dataArray['data']['id'];
 
@@ -33,27 +33,7 @@ class MercadoPagoWebHookController extends Controller
             }
         } else {
             Log::error('Error de tipos de dato: ');
-        }
-
-
-        // Asumiendo que el evento del webhook es relacionado con una suscripción
-        if ($dataArray['type'] === 'subscription') {
-            $id_pago = $dataArray['data']['id'];
-
-            $save = Pay::create([
-                'status' => 'approved',
-                'pago_id' => $id_pago, //con esta id se puede gestionar los datos en mercado pago
-                'tipo_pago' => 'Producto', //$request->payment_type
-            ]);
-
-            if ($save) {
-                Log::info('Datos de la suscripcion guardados correctamente');
-                return response()->json(['status' => 'ok']);
-            } else {
-                Log::error('Datos de la suscripcion no guardados');
-            }
-        } else {
-            Log::error('Error de tipos de dato: ');
         }*/
+
     }
 }
