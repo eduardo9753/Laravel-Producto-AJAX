fe8fbc8855f4c162ef160e85f6bf7b9530637c19
Eduardo <picis_9716@hotmail.com>
2023-09-27 11:08:00 -0500

    PAGINA WEB FRUTIMANIA - AJAX added mercadopago - stripe v.1.5

--- app/Http/Controllers/cliente/MercadoPagoSuscripcionControllerCliente.php
+++ app/Http/Controllers/cliente/MercadoPagoSuscripcionControllerCliente.php
@@ -79,22 +79,18 @@ class MercadoPagoSuscripcionControllerCliente extends Controller
 
     public function success(Request $request)
     {
+        //puedes registrarlo en la base de datos
+        $save = Pay::create([
+            'status' => $request->status,
+            'pago_id' => $request->preapproval_id, //id es distinto de un pago normal , es usado para pagos recurrentes 
+            'tipo_pago' => 'Suscripcion', // $request->payment_type
+        ]);
 
-        dd($request);
-        /*puedes registrarlo en la base de datos
-        if ($request->status === 'approved') {
-            $save = Pay::create([
-                'status' => $request->status,
-                'pago_id' => $request->payment_id, //con esta id se puede gestionar los datos en mercado pago
-                'tipo_pago' => 'Suscripcion',// $request->payment_type
-            ]);
-
-            if ($save) {
-                return redirect()->route('inicio.index')->with('pay', 'Se realizó el pago de tu suscripcion correctamente');
-            } else {
-                return redirect()->route('inicio.index')->with('nopay', 'No se realizó el pago de tu suscripcion correctamente');
-            }
-        }*/
+        if ($save) {
+            return redirect()->route('inicio.index')->with('pay', 'Se realizó el pago de tu suscripcion correctamente');
+        } else {
+            return redirect()->route('inicio.index')->with('nopay', 'No se realizó el pago de tu suscripcion correctamente');
+        }
         //else para las demos estados 
     }
 
--- public/js/cliente/mercadoPagoSuscripcion.js
+++ public/js/cliente/mercadoPagoSuscripcion.js
@@ -12,35 +12,13 @@ window.addEventListener('DOMContentLoaded', () => {
             beforeSend: function () { $('#form-cart-venta-suscripcion').find('#checkout-btn').attr('disabled', true) }, //desabilitamos
 
             success: function (data) {
-                console.log('Datos init_point: ', data.msg.init_point);
+                //console.log('Datos init_point: ', data.msg.init_point);
                 //CREANDO EL BOTON DE PAGO MERCADOPAGO
                 var link = data.msg.init_point;
                 $('#link-suscripcion-mercadopago').attr('href', link);
             }
         });
     });
-
-
-
-    //<a mp-mode="dftl" href="https://www.mercadopago.com.pe/subscriptions/checkout?preapproval_plan_id=2c9380848ab2cb05018aba814d4c05a0" name="MP-payButton" class='blue-ar-l-rn-none'>Suscribirme</a>
-
-    (function () {
-        function $MPC_load() {
-            window.$MPC_loaded !== true && (function () {
-                var s = document.createElement("script");
-                s.type = "text/javascript";
-                s.async = true;
-                s.src = document.location.protocol + "//secure.mlstatic.com/mptools/render.js";
-                var x = document.getElementsByTagName('script')[0];
-                x.parentNode.insertBefore(s, x);
-                window.$MPC_loaded = true;
-            })();
-        }
-        window.$MPC_loaded !== true ? (window.attachEvent ? window.attachEvent('onload', $MPC_load) : window.addEventListener('load', $MPC_load, false)) : null;
-    })();
-
-
-
 });
 
 
