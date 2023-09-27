30cdde6f0e5d6490bd2cc0c4eee3ccd5345fb1c4
Eduardo <picis_9716@hotmail.com>
2023-09-26 21:04:14 -0500

    PAGINA WEB FRUTIMANIA - AJAX added mercadopago - stripe v.1.5

--- app/Http/Controllers/admin/mercadopago/MercadoPagoWebHookController.php
+++ app/Http/Controllers/admin/mercadopago/MercadoPagoWebHookController.php
@@ -11,15 +11,14 @@ class MercadoPagoWebHookController extends Controller
 {
     public function index(Request $request)
     {
-        /* Obtén el contenido de la solicitud (JSON del webhook)
+        //Obtén el contenido de la solicitud (JSON del webhook)
         $payload = $request->getContent();
-        dd($payload);
 
-         Obten la firma HMAC enviada por MercadoPago
+        //Obten la firma HMAC enviada por MercadoPago
         $signature = $request->header('x-signature');
 
         // Verifica la firma HMAC
-        $secretKey = config('mercadopago.client_secret');
+        $secretKey = config('mercadopago.token');
 
         if (hash_hmac('sha256', $payload, $secretKey) === $signature) {
             // La firma es válida, el webhook es auténtico
@@ -28,6 +27,14 @@ class MercadoPagoWebHookController extends Controller
             $data = json_decode($payload, true);
             if ($data['type'] === 'payment') {
                 // El evento es un pago completado, procesa la información
+                $paymentId = $data['data']['id'];
+                $orderId = $data['data']['order']['id'];
+                $status = $data['data']['status'];
+                Pay::create([
+                    'status' => $status,
+                    'pago_id' => $paymentId, //con esta id se puede gestionar los datos en mercado pago
+                    'tipo_pago' => 'Producto', //$request->payment_type
+                ]);
             }
 
             // Responde a la notificación con un código 200 OK
@@ -35,29 +42,6 @@ class MercadoPagoWebHookController extends Controller
         } else {
             // La firma no es válida, ignora la notificación o maneja el error
             return response()->json(['message' => 'Invalid webhook request'], 400);
-        }*/
-
-        // Verificar la autenticidad de la solicitud de Mercado Pago aquí si es necesario
-        
-        $payload = json_decode($request->getContent(), true);
-
-        // Asumiendo que el evento del webhook es un pago exitoso
-        if ($payload['type'] === 'payment') {
-            $paymentId = $payload['data']['id'];
-            $orderId = $payload['data']['order']['id'];
-            $status = $payload['data']['status'];
-            
-            Pay::create([
-                'status' => $status,
-                'pago_id' => $paymentId, //con esta id se puede gestionar los datos en mercado pago
-                'tipo_pago' => 'Producto', //$request->payment_type
-            ]);
-
-            // Registra la notificación en el registro (log)
-            Log::info('Mercado Pago Webhook received for payment ' . $paymentId . ' - Status: ' . $status);
         }
-
-        // Responde a la solicitud de Mercado Pago para confirmar la recepción
-        return response()->json(['status' => 'ok']);
     }
 }
