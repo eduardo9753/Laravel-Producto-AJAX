fa4e9f8688fbcc4e235f150d03ae32d5641914a7
Eduardo <picis_9716@hotmail.com>
2023-09-26 21:57:28 -0500

    PAGINA WEB FRUTIMANIA - AJAX added mercadopago - stripe v.1.5

--- app/Http/Controllers/admin/mercadopago/MercadoPagoWebHookController.php
+++ app/Http/Controllers/admin/mercadopago/MercadoPagoWebHookController.php
@@ -11,36 +11,18 @@ class MercadoPagoWebHookController extends Controller
 {
     public function index(Request $request)
     {
-        //Obtén el contenido de la solicitud (JSON del webhook)
-        $payload = $request->getContent();
-
-        //Obten la firma HMAC enviada por MercadoPago
-        $signature = $request->header('x-signature');
-
-        // Verifica la firma HMAC
         $secretKey = config('mercadopago.token');
-
-
+        $payload = file_get_contents('php://input');
+        $signature = $request->header('x-signature');
 
 
         Log::info('FIRMA ENVIADA POR MERCADOPAGO ' . $signature . 'FIRMA MIA: ' . hash_hmac('sha256', $payload, $secretKey));
-        if (hash_hmac('sha256', $payload, $secretKey) === $signature) {
-            // La firma es válida, el webhook es auténtico
-
-            // Procesa la notificación y toma las acciones necesarias
-            $data = json_decode($payload, true);
-
-
-            Log::info('DATOS DE LA COMPRAR' . $payload . '');
-            Log::info('FIRMA ENVIADA POR MERCADOPAGO ' . $signature . '');
-            Log::info('ID DEL PAGO ' . $data['data']['id'] . '');
-            Log::info('ESTATUS DEL PAGO ' . $data['data']['status'] . '');
-            Log::info('TIPO DEL PAGO ' . $data['type'] . '');
-            Log::info('Mercado Pago Webhook received for payment ' . $data['data']['id'] . ' - Status: ' . $data['data']['status']);
-        } else {
-            // La firma no es válida, ignora la notificación o maneja el error
-            //return response()->json(['message' => 'Invalid webhook request'], 400);
+        if (!$this->isValidSignature($payload, $secretKey, $signature)) {
             Log::info('LA FIRMA NO ES IGUAL');
+        } else {
+            // Procesa la notificación de Mercado Pago
+            $data = json_decode($payload, true);
+            Log::info('LA FIRMA ES CORRECTA' . $data);
         }
     }
 }
