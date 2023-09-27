a05c048b620785ed569441f1af8dcad456bd885b
Eduardo <picis_9716@hotmail.com>
2023-09-27 12:00:17 -0500

    PAGINA WEB FRUTIMANIA - AJAX added mercadopago - stripe v.1.5

--- app/Http/Controllers/admin/mercadopago/MercadoPagoSuscripcionController.php
+++ app/Http/Controllers/admin/mercadopago/MercadoPagoSuscripcionController.php
@@ -30,7 +30,7 @@ class MercadoPagoSuscripcionController extends Controller
         $accessToken = config('mercadopago.token'); // Reemplaza con tu token real
 
         // Construye la URL del endpoint de cancelación de suscripción
-        $url = "https://api.mercadopago.com/preapproval/$request->preapprovalId/cancel";
+        $url = "https://api.mercadopago.com/preapproval/$request->preapprovalId";
 
         try {
             // Realiza una solicitud POST a la URL de cancelación
