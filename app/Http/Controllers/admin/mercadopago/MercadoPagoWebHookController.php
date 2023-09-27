f9775ca24cb882eead2cd6b8da3037b103540798
Eduardo <picis_9716@hotmail.com>
2023-09-26 23:07:55 -0500

    PAGINA WEB FRUTIMANIA - AJAX added mercadopago - stripe v.1.5

--- app/Http/Controllers/admin/mercadopago/MercadoPagoWebHookController.php
+++ app/Http/Controllers/admin/mercadopago/MercadoPagoWebHookController.php
@@ -14,16 +14,25 @@ class MercadoPagoWebHookController extends Controller
         $payload = json_decode($request->getContent(), true);
 
         // Asumiendo que el evento del webhook es un pago exitoso
-        if ($payload['type'] === 'payment') {
-            $paymentId = $payload['data']['id'];
-            // Puedes acceder a más datos según sea necesario
-
-            // Aquí puedes actualizar el estado de la orden en tu base de datos
-            // Puedes agregar lógica adicional según tus necesidades
+        if ($payload['type'] === 'payment' && $payload['data']['status'] === 'approved') {
+            // Aquí puedes agregar lógica para procesar el pago exitoso
+            // Por ejemplo, puedes enviar una notificación por correo electrónico
+            $this->sendPaymentNotification($payload['data']);
+            Log::info('type:  ' . $payload['type']);
+            Log::info('status:  ' . $payload['data']['status']);
+            Log::info('id pago:  ' . $payload['data']['id']);
         }
 
-        Log::info('Datos:  ' . json_encode($payload['data']));
-        Log::info('paymentId: ' . $paymentId);
-        Log::info('type:  ' . $payload['type']);
+        // Responde a la solicitud de Mercado Pago para confirmar la recepción
+        return response()->json(['status' => 'ok']);
+    }
+
+    private function sendPaymentNotification($data)
+    {
+        Log::info('Datos:  ' . $data);
+        // Aquí puedes implementar la lógica para enviar una notificación por correo electrónico
+        // Por ejemplo, puedes utilizar el sistema de notificaciones de Laravel para enviar el correo
+        // $user = User::find($data['customer_id']);
+        // $user->notify(new PaymentNotification($data));
     }
 }
