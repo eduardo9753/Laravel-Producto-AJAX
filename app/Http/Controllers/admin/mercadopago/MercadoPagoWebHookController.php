f9b64b0e6c6d20899728af58aa8c11f7ba882d53
Eduardo <picis_9716@hotmail.com>
2023-09-26 21:26:27 -0500

    PAGINA WEB FRUTIMANIA - AJAX added mercadopago - stripe v.1.5

--- app/Http/Controllers/admin/mercadopago/MercadoPagoWebHookController.php
+++ app/Http/Controllers/admin/mercadopago/MercadoPagoWebHookController.php
@@ -25,7 +25,7 @@ class MercadoPagoWebHookController extends Controller
 
             // Procesa la notificación y toma las acciones necesarias
             $data = json_decode($payload, true);
-            if ($data['type'] === 'payment') {
+            /*if ($data['type'] === 'payment') {
                 // El evento es un pago completado, procesa la información
                 $paymentId = $data['data']['id'];
                 $status = $data['data']['status'];
@@ -34,13 +34,17 @@ class MercadoPagoWebHookController extends Controller
                     'pago_id' => $paymentId, //con esta id se puede gestionar los datos en mercado pago
                     'tipo_pago' => 'Producto', //$request->payment_type
                 ]);
-            }
+            }*/
+
 
             // Responde a la notificación con un código 200 OK
-            return response()->json(['message' => 'Webhook received'], 200);
+            //return response()->json(['message' => 'Webhook received'], 200);
+            Log::info('datos de captura ' . $payload . '');
+            Log::info('Firma ' . $signature . '');
+            Log::info('Mercado Pago Webhook received for payment ' . $data['data']['id'] . ' - Status: ' . $data['data']['status']);
         } else {
             // La firma no es válida, ignora la notificación o maneja el error
-            return response()->json(['message' => 'Invalid webhook request'], 400);
+            //return response()->json(['message' => 'Invalid webhook request'], 400);
         }
     }
 }
